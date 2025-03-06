f15481b13da1603a5b23d011396621fa2dfa9e5f
K-Kizuku <kotani.kizuku@gmail.com>
2025-03-07 02:26:17 +0900

    webRTC仮実装

--- front/src/app/pages/index/page.tsx
+++ front/src/app/pages/index/page.tsx
@@ -1,25 +1,29 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import BubbleVis from "./../../components/BubbleVis/page"
-import { bubbleData } from "./../../data/dummy"
-import styles from "./index.module.css"
+import { useState } from "react";
+import BubbleVis from "./../../components/BubbleVis/page";
+import { bubbleData } from "./../../data/dummy";
+import styles from "./index.module.css";
+import BackgroundVideo from "@/app/components/BackgroundVideo";
 
 export default function Home() {
-  const [data, setData] = useState(bubbleData)
+  const [data, setData] = useState(bubbleData);
 
   const handleLike = (id: number) => {
     setData((prevData) =>
-      prevData.map((bubble) => (bubble.id === id ? { ...bubble, likes: bubble.likes + 1 } : bubble)),
-    )
-  }
+      prevData.map((bubble) =>
+        bubble.id === id ? { ...bubble, likes: bubble.likes + 1 } : bubble
+      )
+    );
+  };
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>デモ</h1>
         <BubbleVis bubbles={data} onLike={handleLike} />
+        <BackgroundVideo />
       </main>
     </div>
-  )
+  );
 }
